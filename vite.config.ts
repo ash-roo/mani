fd13745813e02fe07c7ae0a2becdbf32242d0a20
ash-roo <aakash.n0dev@gmail.com>
2025-06-10 11:23:09 +0530

    New

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/manikanda-prabu",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
